05ddcfb167c08b477c0f44b19177468aa899f890
David Weiner <dweiner@predictivescience.com>
2014-10-03 18:00:36 -0400

    Issue #3 -- Finished up the basic CRUD functions for chapters.  Additional chapter types can be easily added at a later date, as well as additional chapter information.

--- app/views/chapter/show.blade.php
+++ app/views/chapter/show.blade.php
@@ -10,16 +10,16 @@
         <tbody>
                 <tr>
                     <td>Chapter Name</td>
-                    <td>{{{ $detail->chapter_name }}}{{{ isset($detail->hull_number) ? ' (' . $detail->hull_number . ')' : '' }}}</td>
+                    <td>{{{ $detail->chapter_name }}}{{{ isset($detail->hull_number) ? ' (' . $detail->hull_number . ')' : '' }}} <a href="/chapter/{{{ $detail->_id }}}/edit">Edit</a></td>
                 </tr>
                 <tr>
                     <td>Chapter Type</td>
                     <td>{{{ $detail->chapter_type }}}</td>
                 </tr>
-                @if ($higher !== false)
+                @if ($higher)
                 <tr>
                     <td>Assigned To:</td>
-                    <td><a href="/chapter/{{{ $higher->_id }}}/show">{{{ $higher->chapter_name }}}{{{ isset($higher->hull_number) ? ' (' . $higher->hull_number . ')' : '' }}}</a></td>
+                    <td><a href="/chapter/{{{ $higher->_id }}}">{{{ $higher->chapter_name }}}{{{ isset($higher->hull_number) ? ' (' . $higher->hull_number . ')' : '' }}}</a></td>
                 </tr>
                 @endif
                 @if (count($includes) > 0)
@@ -27,7 +27,7 @@
                     <td>Includes:</td>
                     <td>
                     @foreach($includes as $chapter)
-                        <a href="/chapter/{{{ $chapter->_id }}}/show">{{{ $chapter->chapter_name }}}{{{ isset($chapter->hull_number) ? ' (' . $chapter->hull_number . ')' : '' }}}</a>&nbsp;
+                        <a href="/chapter/{{{ $chapter->_id }}}">{{{ $chapter->chapter_name }}}{{{ isset($chapter->hull_number) ? ' (' . $chapter->hull_number . ')' : '' }}}</a>&nbsp;
                     @endforeach
                     </td>
                 </tr>
