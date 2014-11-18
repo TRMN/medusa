7379764431d5463ac3c95353ac3c4485600ef171
Eric Dowell <eric@d0we11.com>
2014-11-17 20:30:56 -0600

    Jumped the gun on form selector and added extra check in update method for announcements

--- app/views/announcement/index.blade.php
+++ app/views/announcement/index.blade.php
@@ -23,7 +23,7 @@ Announcements
         <td>{{{ $announcement->updated_at }}}</td>
         <td>{{{ $announcement->is_published ? 'Published' : 'Unpublished' }}}</td>
         <td><button id="{{{ $announcement->id }}}" class="btn" onclick="confirmDelete( '{{ $announcement->id }}' );" data-title="{{{ $announcement->summary }}}">Delete</button>
-            {{ Form::open([ 'url' => route( 'announcement.destroy' , [ $announcement->id ] ) , 'method'=> 'delete' , 'id' => 'deleteFeature' , 'style' => 'display:none;' ]) }}
+            {{ Form::open([ 'url' => route( 'announcement.destroy' , [ $announcement->id ] ) , 'method'=> 'delete' , 'id' => 'deleteAnnouncement-' . $announcement->id , 'style' => 'display:none;' ]) }}
                 {{ Form::submit( 'Delete' )  }}
             {{ Form::close() }}
         </td>
@@ -33,17 +33,17 @@ Announcements
     </tbody>
 </table>
 
-<a href="{{ route('announcement.create')}}" class="button">Create New</a>
+<a href="{{ route('announcement.create') }}" class="button">Create New</a>
 
 @stop
 
-@section('scriptFooter')
+@section( 'scriptFooter' )
 <script>
     var confirmDelete = function( id ) {
         var title = jQuery( '#' + id ).data( 'title' );
         var message = confirm( 'Are you sure you want to delete the ' + title +' announcement?' );
         if( message == true ) {
-          return document.getElementById( 'deleteFeature' ).submit();
+          return document.getElementById( 'deleteAnnouncement-' + id ).submit();
         } else {
           return false;
         }
