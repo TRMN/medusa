b4b4d4100845f007bc54314af7fc2ebb0d8f7442
David Weiner <davidweiner@dreamfactory.com>
2015-06-18 23:40:24 -0400

    Closes #7 Added display of uploaded picture if present.  Added drag and drop photo upload

--- javascript/main.js
+++ javascript/main.js
@@ -1,22 +1,23 @@
-var ManticoreAuth = require( './ManticoreAuth.js' );
-var ManticoreUser = require( './ManticoreUser.js' );
-var ManticoreChapter = require( './ManticoreChapter.js' );
-var ManticoreRegister = require( './ManticoreRegister.js');
+var ManticoreAuth = require('./ManticoreAuth.js');
+var ManticoreUser = require('./ManticoreUser.js');
+var ManticoreChapter = require('./ManticoreChapter.js');
+var ManticoreRegister = require('./ManticoreRegister.js');
+var Dropzone = require('./dropzone.js');
 
-jQuery( document ).ready( function ( $ ) {
+jQuery(document).ready(function ($) {
 
     var authController = new ManticoreAuth();
     var userController = new ManticoreUser();
     var chapterController = new ManticoreChapter();
-	var registerController = new ManticoreRegister();
+    var registerController = new ManticoreRegister();
 
-    if ( $( '.userform' ).length > 0 ) {
+    if ($('.userform').length > 0) {
         userController.initMemberForm();
     }
 
-	if ($('.registerform').length > 0) {
-		registerController.initRegisterForm();
-	}
+    if ($('.registerform').length > 0) {
+        registerController.initRegisterForm();
+    }
 
     var MTIProjectId = '5c059f73-3466-4691-8b9a-27e7d9c1a9c7';
     (function () {
@@ -26,4 +27,18 @@ jQuery( document ).ready( function ( $ ) {
         mtiTracking.src = ('https:' == document.location.protocol ? 'https:' : 'http:') + '//fast.fonts.net/t/trackingCode.js';
         (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(mtiTracking);
     })();
-} );
+
+
+
+    jQuery("#trmnDropzone").dropzone({
+        url: "/api/photo",
+        acceptedFiles: ".png,.gif,.jpg",
+        uploadMultiple: false,
+        init: function() {
+            this.on("complete", function (file) {
+                jQuery('#reload-form').val('yes');
+                jQuery('#user').submit();
+            });
+        }
+    });
+});
