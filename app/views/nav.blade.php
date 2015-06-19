ebdf552eb911a53d5ce97f93e20618c1fbc3fb34
David Weiner <davidweiner@dreamfactory.com>
2015-06-18 23:40:24 -0400

    Closes #7 Added display of uploaded picture if present.  Added drag and drop photo upload

--- app/views/nav.blade.php
+++ app/views/nav.blade.php
@@ -1,6 +1,6 @@
 <div id="left-nav">
     @if(Auth::check())
-        <div class="button lnav">MEMBER</div>
+        <div class="nav-header lnav">MEMBER</div>
         <div class="rnav">
 
             <a href="/home">Service Record</a><br />
@@ -8,17 +8,18 @@
 
         </div>
 
-        <div class="button lnav">BuPers (5SL)</div>
+        <div class="nav-header lnav">BuPers (5SL)</div>
         <div class="rnav">
-            <a href="{{ route('user.index') }}">Users</a>
+            <a href="{{ route('user.index') }}">Users</a><br/>
+            <a href="{{ route('user.create') }}">Add User</a>
         </div>
 
-        <div class="button lnav">CHAPTERS</div>
+        <div class="nav-header lnav">CHAPTERS</div>
         <div class="rnav">
             <a href="{{ route('chapter.index') }}">Chapter List</a>
         </div>
 
-        <div class="button lnav">ADMIRALTY</div>
+        <div class="nav-header lnav">ADMIRALTY</div>
         <div class="rnav">
             <a href="{{ route('announcement.index') }}">Announcements</a>
         </div>
