ae04f5f448ddfed1f4748346a5471778553ee6fe
David Weiner <davidweiner@dreamfactory.com>
2015-06-24 20:12:55 -0400

    Closes #35 Styled chapter list to match design document provided by Bob

--- app/views/chapter/index.blade.php
+++ app/views/chapter/index.blade.php
@@ -6,23 +6,37 @@ Chapters
 
 @section('content')
     <h1>Chapter List</h1>
-    <table class="table table-striped">
+    <table id="chapterList" class="compact row-border stripe" width="75%">
         <thead>
             <tr>
                 <th>Chapter Name</th>
                 <th>Chapter Type</th>
-                <th></th>
+                <th width="12%">Actions</th>
             </tr>
         </thead>
         <tbody>
             @foreach( $chapters as $chapter )
                 <tr>
-                    <td><a href="/chapter/{{{ $chapter->_id }}}">{{{ $chapter->chapter_name }}}{{{ isset($chapter->hull_number) ? ' (' . $chapter->hull_number . ')' : '' }}}</a></td>
+                    <td><a href="/chapter/{{{ $chapter->_id }}}">{{{ $chapter->chapter_name }}}
+                            <?php
+                            $chapterType = Chapter::getChapterType($chapter->_id);
+                            ?>
+                            @if($chapterType == "ship" || $chapterType == "station")
+                                {{{ isset($chapter->hull_number) ? ' (' . $chapter->hull_number . ')' : '' }}}
+                            @endif
+                        </a></td>
                     <td>{{{ $chapter->chapter_type }}}</td>
-                    <td class="right"><a href="/chapter/{{{ $chapter->_id }}}/edit" class="button tiny">Edit</a> <button data-mongoid="{{$chapter->_id}}" class="button tiny delete-chapter">Delete</button></td>
+                    <td width="12%"><a href="/chapter/{{{ $chapter->_id }}}/edit" class="tiny">Edit</a> <a href="#" data-mongoid="{{$chapter->_id}}" class="tiny delete-chapter">Delete</a></td>
                 </tr>
             @endforeach
         </tbody>
+        <tfoot>
+        <tr>
+            <th>Chapter Name</th>
+            <th>Chapter Type</th>
+            <th width="12%">Actions</th>
+        </tr>
+        </tfoot>
     </table>
     <a href="{{ route('chapter.create') }}" class="button">Create New Chapter</a>
 @stop
--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -1,15 +1,33 @@
 <!DOCTYPE html>
 <html>
 <head>
+
     <title>@yield('pageTitle') | Royal Manticoran Navy Database</title>
-    <link rel="stylesheet" type="text/css"
-          href="//cdnjs.cloudflare.com/ajax/libs/foundation/5.4.0/css/normalize.min.css">
-    <link rel="stylesheet" type="text/css"
-          href="//cdnjs.cloudflare.com/ajax/libs/foundation/5.4.0/css/foundation.min.css">
-    <link rel="stylesheet" type="text/css"
-          href="//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.10.4/css/jquery.ui.datepicker.min.css">
-    <link rel="stylesheet" type="text/css"
-          href="//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.10.4/css/jquery-ui.min.css">
+    @if(App::environment('local') === true)
+        <link rel="stylesheet" type="text/css" href="{{{ $serverUrl }}}/css/normalize.min.css">
+        <link rel="stylesheet" type="text/css" href="{{{ $serverUrl }}}/css/foundation.min.css">
+        <link rel="stylesheet" type="text/css" href="{{{ $serverUrl }}}/css/jquery.ui.datepicker.min.css">
+        <link rel="stylesheet" type="text/css" href="{{{ $serverUrl }}}/css/jquery-ui.min.css">
+        <link rel="stylesheet" type="text/css" href="{{{ $serverUrl }}}/css/jquery.dataTables.min.css">
+        <link rel="stylesheet" type="text/css" href="{{{ $serverUrl }}}/css/dataTables.foundation.css">
+        <link rel="stylesheet" type="text/css" href="{{{ $serverUrl }}}/css/dataTables.jqueryui.css">
+        <link rel="stylesheet" type="text/css" href="{{{ $serverUrl }}}/css/jquery-ui.css">
+    @else
+        <link rel="stylesheet" type="text/css"
+              href="//cdnjs.cloudflare.com/ajax/libs/foundation/5.4.0/css/normalize.min.css">
+        <link rel="stylesheet" type="text/css"
+              href="//cdnjs.cloudflare.com/ajax/libs/foundation/5.4.0/css/foundation.min.css">
+        <link rel="stylesheet" type="text/css"
+              href="//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.10.4/css/jquery.ui.datepicker.min.css">
+        <link rel="stylesheet" type="text/css"
+              href="//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.10.4/css/jquery-ui.min.css">
+        <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.7/css/jquery.dataTables.min.css">
+        <link rel="stylesheet" type="text/css"
+              href="//cdn.datatables.net/plug-ins/1.10.7/integration/foundation/dataTables.foundation.css">
+        <link rel="stylesheet" type="text/css" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css"
+        <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/plug-ins/1.10.7/integration/jqueryui/dataTables.jqueryui.css">
+
+    @endif
     <link rel="stylesheet" type="text/css" href="{{{ $serverUrl }}}/css/main.css">
 
 </head>
@@ -37,22 +55,40 @@
     </div>
 
     <footer class="row">
-        <p>Copyright &copy; 2008 &ndash; {{{ date('Y') }}} The Royal Manticoran Navy: The Official Honor Harrington Fan Association,
+        <p>Copyright &copy; 2008 &ndash; {{{ date('Y') }}} The Royal Manticoran Navy: The Official Honor Harrington Fan
+            Association,
             Inc. Some Rights Reserved.
             Honor Harrington and all related materials are &copy; David Weber.</p>
+
         <p>Medusa Version {{{ Config::get('app.version') }}}</p>
     </footer>
 </div>
-
-<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/foundation/5.4.0/js/foundation.min.js"></script>
-<script type="text/javascript"
-        src="//cdnjs.cloudflare.com/ajax/libs/foundation/5.5.2/js/foundation/foundation.topbar.min.js"></script>
-<script type="text/javascript"
-        src="//cdnjs.cloudflare.com/ajax/libs/foundation/5.5.2/js/foundation/foundation.accordion.min.js"></script>
-<script type="text/javascript"
-        src="//cdnjs.cloudflare.com/ajax/libs/foundation/5.5.2/js/foundation/foundation.reveal.min.js"></script>
-<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.10.4/jquery-ui.js"></script>
+@if(App::environment('local') === true)
+    <script type="text/javascript" src="{{{ $serverUrl }}}/js/jquery.min.js"></script>
+    <script type="text/javascript" src="{{{ $serverUrl }}}/js/foundation.min.js"></script>
+    <script type="text/javascript" src="{{{ $serverUrl }}}/js/foundation.topbar.min.js"></script>
+    <script type="text/javascript" src="{{{ $serverUrl }}}/js/foundation.accordion.min.js"></script>
+    <script type="text/javascript" src="{{{ $serverUrl }}}/js/foundation.reveal.min.js"></script>
+    <script type="text/javascript" src="{{{ $serverUrl }}}/js/jquery-ui.js"></script>
+    <script type="text/javascript" src="{{{ $serverUrl }}}/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="{{{ $serverUrl }}}/js/dataTables.foundation.js"></script>
+    <script type="text/javascript" src="{{{ $serverUrl }}}/js/dataTables.jqueryui.js"></script>
+@else
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script type="text/javascript"
+            src="//cdnjs.cloudflare.com/ajax/libs/foundation/5.4.0/js/foundation.min.js"></script>
+    <script type="text/javascript"
+            src="//cdnjs.cloudflare.com/ajax/libs/foundation/5.5.2/js/foundation/foundation.topbar.min.js"></script>
+    <script type="text/javascript"
+            src="//cdnjs.cloudflare.com/ajax/libs/foundation/5.5.2/js/foundation/foundation.accordion.min.js"></script>
+    <script type="text/javascript"
+            src="//cdnjs.cloudflare.com/ajax/libs/foundation/5.5.2/js/foundation/foundation.reveal.min.js"></script>
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.10.4/jquery-ui.js"></script>
+    <script type="text/javascript" src="//cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
+    <script type="javascript"
+            src="//cdn.datatables.net/plug-ins/1.10.7/integration/foundation/dataTables.foundation.js"></script>
+    <script type="text/javascript" src="//cdn.datatables.net/plug-ins/1.10.7/integration/jqueryui/dataTables.jqueryui.js"></script>
+@endif
 <script>
     jQuery(document).foundation();
 </script>
