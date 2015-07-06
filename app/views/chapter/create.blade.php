4b5ae54a6786dc7aa0ea111e7c165d94422b3cf8
David Weiner <davidweiner@dreamfactory.com>
2015-07-05 21:53:48 -0400

    Styled user list.  Added tabs by branch.

--- app/views/chapter/create.blade.php
+++ app/views/chapter/create.blade.php
@@ -1,11 +1,11 @@
 @extends('layout')
 
 @section('pageTitle')
-Create a New Chapter
+Create a New Ship/Unit
 @stop
 
 @section('content')
-<h2>Create a New Chapter</h2>
+<h2>Create a New Ship/Unit</h2>
 <?php
 if (count($errors->all())) {
 echo "<p>Please correct the following errors:</p>\n<ul>\n";
@@ -17,19 +17,31 @@ echo "<p>Please correct the following errors:</p>\n<ul>\n";
 echo "</ul>\n";
 ?>
 {{ Form::model( $chapter, [ 'route' => [ 'chapter.store' ] ] ) }}
-<div class="form-group">
+<div class="row">
+    <div class="small-6 columns ninety Incised901Light end">
     {{ Form::label('chapter_name', 'Chapter Name') }} {{ Form::text('chapter_name') }}
+        </div>
 </div>
-<div class="form-group">
+<div class="row">
+    <div class="small-6 columns ninety Incised901Light end">
     {{ Form::label('Chapter Type', 'Chapter Type') }} {{ Form::select('chapter_type', $chapterTypes) }}
+        </div>
 </div>
-<div class="form-group">
-    {{ Form::label('hull_number', 'Hull Number (if appropriate)') }} {{ Form::text('hull_number') }}
-    {{ Form::label('ship_class', 'Ship Class') }} {{ Form::text('ship_class') }}
+<div class="row">
+    <div class="small-6 columns ninety Incised901Light end">
+    {{ Form::label('hull_number', 'Hull Number/Chapter Identifier (if appropriate)') }} {{ Form::text('hull_number') }}
+        </div>
+    </div>
+<div class="row">
+    <div class="small-6 columns ninety Incised901Light end">
+    {{ Form::label('ship_class', 'Ship Class (if appropriate') }} {{ Form::text('ship_class') }}
+        </div>
 </div>
-<div class="form-group">
-    {{ Form::label('Assigned To', 'Assigned To') }} {{ Form::select('assigned_to', $chapterList) }}
+<div class="row">
+    <div class="small-6 columns ninety Incised901Light end">
+    {{ Form::label('Assigned To', 'Parent Chapter/Unit') }} {{ Form::select('assigned_to', $chapterList) }}
+        </div>
 </div>
-{{ Form::submit( 'Save', [ 'class' => 'button'] ) }}
+{{ Form::submit( 'Save', [ 'class' => 'button round'] ) }}
 {{ Form::close() }}
 @stop
--- app/views/nav.blade.php
+++ app/views/nav.blade.php
@@ -8,15 +8,16 @@
 
         </div>
 
-        <div class="nav-header lnav">BuPers (5SL)</div>
+        <div class="nav-header lnav">BuShips (3SL)</div>
         <div class="rnav">
-            <a href="{{ route('user.index') }}">Users</a><br/>
-            <a href="{{ route('user.create') }}">Add User</a>
+            <a href="{{ route('chapter.index') }}">Ship/Unit List</a><br/>
+            <a href="{{{route('chapter.create')}}}">Add Ship/Unit</a>
         </div>
 
-        <div class="nav-header lnav">CHAPTERS</div>
+        <div class="nav-header lnav">BuPers (5SL)</div>
         <div class="rnav">
-            <a href="{{ route('chapter.index') }}">Chapter List</a>
+            <a href="{{ route('user.index') }}">List Members</a><br/>
+            <a href="{{ route('user.create') }}">Add Member</a>
         </div>
 
         <div class="nav-header lnav">ADMIRALTY</div>
