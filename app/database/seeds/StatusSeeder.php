b70efb5cc3166ce55743555808a9599adf5fd90a
David Weiner <davidweiner@dreamfactory.com>
2015-09-16 21:25:38 -0400

    Added Audit trails, latest exports, version changed to 0.9

--- app/database/seeds/StatusSeeder.php
+++ app/database/seeds/StatusSeeder.php
@@ -3,6 +3,8 @@
 class StatusSeeder extends Seeder
 {
 
+    use \Medusa\Audit\MedusaAudit;
+
     public function run()
     {
         DB::collection('statuses')->delete();
@@ -11,6 +13,16 @@ class StatusSeeder extends Seeder
 
         foreach ($statuses as $status) {
             $this->command->comment('Adding ' . $status);
+
+            $this->writeAuditTrail(
+                'db:seed',
+                'create',
+                'statuses',
+                null,
+                json_encode(['status' => $status]),
+                'status'
+            );
+
             Status::create(["status" => $status]);
         }
     }
