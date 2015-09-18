aca9567200ffff7c18d0d04745365d9f64f6511f
David Weiner <davidweiner@dreamfactory.com>
2015-09-17 20:20:59 -0400

    Medusa v 0.9.1
    Removed striping from datatables to increase legibility
    
    DB Migrations to force all email addresses to lower case, to make echelons unjoinable, add fleet command, deputy fleet commander, fleet bosun and flag lt. billets
    
    Updated save and update routines to make sure that email addresses are always lowercase and that echelons are saved as being unjoinable
    
    Added billet to ship/unit roster
    
    Updated command crew routines to include the new fleet command staff billets and updated the view to use them when appropriate
    
    Added artisan command to add CO permissions to a Fleet CO or other designated individual
    
    Added fancy error page

---
+++ app/views/error.blade.php
@@ -0,0 +1,26 @@
+@extends('layout')
+
+@section('pageTitle')
+    Paging Sir Horace!
+@stop
+
+@section('content')
+<div class="row">
+    <div class="columns small-12">
+        <h2>Paging Sir Horace! We've encountered a bug in the Medusa-class database. Say again, we've encountered a bug in the Medusa-class database.</h2>
+
+        <p>Please report this error via email (<a href="mailto:bolthole@trmn.org">bolthole@trm.org</a>) or the <a href="https://bolthole.trmn.org">Web Portal</a>.
+            Be sure to report exactly what you were doing when the error occurred or the exact steps required to duplicate the error and the error text below.</p>
+
+        <p>Thank You,<br /><br />
+        The Medusa Dev Team</p>
+    </div>
+</div>
+
+<div class="row">
+    <div class="alert-box alert" data-alert>
+        {{ $error }}
+    </div>
+</div>
+
+@stop
