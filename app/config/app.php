b20f6ab626d40be5cdd7f874890dd8b179e55eaa
David Weiner <davidweiner@dreamfactory.com>
2015-12-10 12:07:22 -0500

    Updated to deal with new RMMC sheet headers

--- app/config/app.php
+++ app/config/app.php
@@ -205,6 +205,6 @@ return [
         'SimpleCaptcha' => 'Iorme\SimpleCaptcha\Facades\SimpleCaptcha',
     ],
 
-    'version' => "King Roger III (Medusa DB v1.1.9)",
+    'version' => "King Roger III (Medusa DB v1.1.10)",
 
 ];
