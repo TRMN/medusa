bb7e6ed2165639e7c8e79bf57bd5cd2ba6ae7d1f
David Weiner <davidweiner@dreamfactory.com>
2016-01-24 00:18:23 -0500

    Added rank equiv charts.  Updated rank titles.  Added RMMM and RMASC

--- app/config/app.php
+++ app/config/app.php
@@ -205,6 +205,6 @@ return [
         'SimpleCaptcha' => 'Iorme\SimpleCaptcha\Facades\SimpleCaptcha',
     ],
 
-    'version' => "King Roger III (Medusa DB v1.1.18)",
+    'version' => "King Roger III (Medusa DB v1.1.19)",
 
 ];
