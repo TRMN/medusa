9f6778f6b2ce267bada3fa368bc38650cb3d0b58
eplossl <eplossl@laptop>
2016-02-04 20:57:18 -0500

    Added sass compile option to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,17 @@ var browserify = require( 'browserify' );
 var source = require( 'vinyl-source-stream' );
 var buffer = require( 'vinyl-buffer' );
 var uglify = require( 'gulp-uglify' );
+var sass = require('gulp-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public/css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
 
 gulp.task( 'browserify', function() {
     return browserify( './javascript/main.js')
