4c4e331a8c56cd1229ce172ba487a81f70bd27c0
Dave <david.l.weiner30030@gmail.com>
2016-10-24 17:49:45 -0400

    Possible fix for + in email addresses

--- app/config/app.php
+++ app/config/app.php
@@ -209,5 +209,5 @@ return [
         'QrCode'              => 'SimpleSoftwareIO\QrCode\Facades\QrCode',
 
     ],
-    'version' => "Trevor's Star (Project MEDUSA v1.3.5)",
+    'version' => "Trevor's Star (Project MEDUSA v1.3.6)",
 ];
