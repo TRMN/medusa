266a1abef198f9680d777a10f520988a73810030
Dave <david.l.weiner30030@gmail.com>
2016-11-14 12:35:28 -0500

    Possible fix for mixed case logins in the app

--- app/config/app.php
+++ app/config/app.php
@@ -209,5 +209,5 @@ return [
         'QrCode'              => 'SimpleSoftwareIO\QrCode\Facades\QrCode',
 
     ],
-    'version' => "Trevor's Star (Project MEDUSA v1.3.11)",
+    'version' => "Trevor's Star (Project MEDUSA v1.3.12)",
 ];
