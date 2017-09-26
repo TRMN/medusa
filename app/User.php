c3316656a25753de5eb8eaaf47d5de7594e776d3
Dave <david.l.weiner30030@gmail.com>
2017-09-26 18:23:48 -0400

    Backport ID card tweaks

--- app/User.php
+++ app/User.php
@@ -1054,13 +1054,16 @@ class User extends Eloquent implements AuthenticatableContract, CanResetPassword
         $idCard =
             Image::make(public_path() . '/images/TRMN-membership-card.png');
 
+        $name = $this->getFullName();
+        $fontSize = strlen($name) < 28 ? 48 : 38;
+
         $idCard->text(
-            $this->getFullName(),
+            $name,
             382,
-            317,
-            function ($font) {
+            330,
+            function ($font) use ($fontSize) {
                 $font->file(public_path() . "/fonts/24bd1ba4-1474-491a-91f2-a13940159b6d.ttf");
-                $font->size(48);
+                $font->size($fontSize);
                 $font->align('center');
             }
         );
