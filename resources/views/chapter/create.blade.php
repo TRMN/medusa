3b893efc64cb891e3de603403ee6c35214da29fd
Dave <david.l.weiner30030@gmail.com>
2017-10-04 22:08:43 -0400

    More bootstrap migration -- Create Ship

--- resources/views/chapter/create.blade.php
+++ resources/views/chapter/create.blade.php
@@ -1,62 +1,64 @@
 @extends('layout')
 
 @section('pageTitle')
-Commission a Ship
+    Commission a Ship
 @stop
 
 @section('content')
-<h2>Commission a Ship</h2>
+    <h2>Commission a Ship</h2>
 
-{!! Form::model( $chapter, [ 'route' => [ 'chapter.store' ] ] ) !!}
-<div class="row">
-    <div class="col-sm-6  ninety Incised901Light ">
-    {!! Form::label('chapter_name', 'Chapter Name') !!} {!! Form::text('chapter_name') !!}
-        </div>
-</div>
-<div class="row">
-    <div class=" col-sm-6  ninety Incised901Light ">
-        {!! Form::label('branch', "Branch") !!} {!! Form::select('branch', $branches, null, ['class' => 'selectize']) !!}
-    </div>
-</div>
-<div class="row">
-    <div class="col-sm-6  ninety Incised901Light ">
-    {!! Form::label('chapter_type', 'Chapter Type') !!} {!! Form::select('chapter_type', $chapterTypes, null, ['class' => 'selectize']) !!}
-        </div>
-</div>
-<div class="row">
-    <div class="col-sm-6  ninety Incised901Light ">
-    {!! Form::label('hull_number', 'Hull Number') !!} {!! Form::text('hull_number') !!}
+    {!! Form::model( $chapter, [ 'route' => [ 'chapter.store' ] ] ) !!}
+    <div class="row">
+        <div class="col-sm-6 Incised901Light form-group">
+            {!! Form::label('chapter_name', 'Chapter Name') !!} {!! Form::text('chapter_name', null, ['class' => 'form-control']) !!}
         </div>
     </div>
-<div class="row">
-    <div class="col-sm-6  ninety Incised901Light ">
-    {!! Form::label('ship_class', 'Ship Class') !!} {!! Form::text('ship_class') !!}
+    <div class="row">
+        <div class=" col-sm-6  ninety Incised901Light ">
+            {!! Form::label('branch', "Branch") !!} {!! Form::select('branch', $branches, null, ['class' => 'selectize']) !!}
         </div>
-</div>
-<div class="row">
-    <div class="col-sm-6  ninety Incised901Light ">
-        {!! Form::label('assigned_to', 'Assigned To') !!} {!! Form::select('assigned_to', $fleets, null, ['class' => 'selectize']) !!}
     </div>
-</div>
-
-<div class="row">
-    <div class="col-sm-6  ninety Incised901Light ">
-        {!! Form::label('commission_date', 'Commission Date (if appropriate)') !!}  {!!Form::date('commission_date')!!}
+    <div class="row">
+        <div class="col-sm-6 Incised901Light form-group">
+            {!! Form::label('chapter_type', 'Chapter Type') !!} {!! Form::select('chapter_type', $chapterTypes, null, ['class' => 'selectize']) !!}
+        </div>
     </div>
-</div>
-
-<div class="row">
-    <div class="col-sm-6  ninety Incised901Light ">
-        {!! Form::label('decommision_date', 'Decomission Date (if appropriate)') !!}  {!!Form::date('decomission_date')!!}
+    <div class="row">
+        <div class="col-sm-6 Incised901Light form-group">
+            {!! Form::label('hull_number', 'Hull Number') !!} {!! Form::text('hull_number', null, ['class' => 'form-control']) !!}
+        </div>
     </div>
-</div>
-
-<div class="row">
-    <div class="col-sm-6  ninety Incised901Light ">
-        {!!Form::checkbox('joinable', true, true) !!} New members and transfers may select this unit
+    <div class="row">
+        <div class="col-sm-6 Incised901Light form-group">
+            {!! Form::label('ship_class', 'Ship Class') !!} {!! Form::text('ship_class', null, ['class' => 'form-control']) !!}
+        </div>
     </div>
-</div>
+    <div class="row">
+        <div class="col-sm-6 Incised901Light form-group">
+            {!! Form::label('assigned_to', 'Assigned To') !!} {!! Form::select('assigned_to', $fleets, null, ['class' => 'selectize']) !!}
+        </div>
+    </div>
 
-{!! Form::submit( 'Save', [ 'class' => 'btn round'] ) !!}
-{!! Form::close() !!}
+    <div class="row">
+        <div class="col-sm-6 Incised901Light form-group">
+            {!! Form::label('commission_date', 'Commission Date (if appropriate)') !!}  {!!Form::date('commission_date', null, ['class' => 'form-control'])!!}
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-6 Incised901Light">
+            {!!Form::checkbox('joinable', true, true) !!} New members and transfers may select this unit
+        </div>
+    </div>
+    <br/>
+    <div class="row">
+        <div class="col-sm-6 Incised901Light text-center">
+            <button type="submit" class="btn btn-success"><span class="fa fa-save"><span
+                            class="Incised901Light"> Save </span></span></button>
+        </div>
+
+    </div>
+
+
+    {!! Form::close() !!}
 @stop
