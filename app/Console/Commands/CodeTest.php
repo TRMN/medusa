32c03e1369e22a07b3e4a16c025f8a1e71c9149b
Dave <david.l.weiner30030@gmail.com>
2018-02-27 12:27:13 -0500

    Testing MCAM issue

--- app/Console/Commands/CodeTest.php
+++ app/Console/Commands/CodeTest.php
@@ -42,24 +42,15 @@ class CodeTest extends Command
     public function fire()
     {
 
-        $awards = ["AFSM", "NCOSCR", "RTR", "NPMC", "NRMC", "NPS", "NRS", "NPE", "NRE", "NPHE", "NRHE", "SSD", "GCM", "MRSM", "MtSM", "MCAM", "QE3GJM", "QE3SJM", "QE3CM", "KR3CM", "GACM", "MHW", "HOSM", "HWC", "MOM", "SAPC", "SvC", "POW", "FEA", "RMUC", "RUCG", "LOH", "NMAM", "NCD", "MSM", "CSM", "CBM", "EM", "GLM", "RM", "MID", "RHDSM", "QBM", "WS", "OCN", "GS", "CGM", "MT", "ME", "MGL", "MR", "DSO", "NS", "OG", "OE", "OGL", "OR", "DGC", "SC", "CE", "CGL", "CR", "KE", "KGL", "KR", "OC", "MC", "KCE", "KCGL", "KCR", "KDE", "KDGL", "KDR", "OM", "GCE", "GCGL", "GCR", "KSK", "AC", "QCB", "PMV"];
+        foreach (\App\User::activeUsers() as $user) {
+            // Check for SWP qualification.  This is for existing SWP's that may
+            // not be recorded, so set isNewAward to false.  Check for SWP first,
+            // because you can get a MCAM unless you have a SWP
+            $user->swpQual(false);
 
-//        foreach ($awards as $award) {
-//            try {
-//                Award::where('code', $award)->firstOrFail();
-//            } catch (\Exception $e) {
-//                $this->info('Could not find ' . $award);
-//            }
-//        }
-
-        $awards = Award::all();
-
-        $awards = array_sort($awards->toArray(), function ($value) {
-            return $value['display_order'];
-        });
-
-        foreach ($awards as $award) {
-            print $award['display_order'] . ' ' . $award['name'] . ' (' . $award['code'] . ")\n";
+            // Check for 1 or more MCAM's.  Again, this is for existing MCAM's
+            // that may not be recorded, so set isNewAward to false.
+            $user->mcamQual(false);
         }
     }
 
