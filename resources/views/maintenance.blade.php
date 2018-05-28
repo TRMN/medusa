8157c5f04d63c452270d9d44342d72f9678f2438
Dave <david.l.weiner30030@gmail.com>
2018-05-28 12:25:33 -0400

    Maint. page updates

--- resources/views/maintenance.blade.php
+++ resources/views/maintenance.blade.php
@@ -3,26 +3,26 @@
 <head>
 
     <title>Manticoran Enhanced Data Usability System Architecture</title>
-    <link rel="stylesheet" type="text/css" href="{!! asset('css/vendor.css') !!}?{{ time() }}">
     <link rel="stylesheet" type="text/css" href="{!! asset('css/app.css') !!}?{{ time() }}">
-    <link rel="stylesheet" type="text/css" href="{!! asset('css/overrides.css') !!}?{{ time() }}">
-    <link href="https://fonts.googleapis.com/css?family=Volkhov" rel="stylesheet">
     <link rel="shortcut icon" href="{!! asset('favicon.ico') !!}">
 </head>
 <body id="down">
-<div style="width: 100%; float: left">
+<div class="container-fluid text-center">
     <div class="row">
-        <div class="col-sm-12 Incised901Black"><br />"Oops!"</div>
+        <div class="col-sm-10 Incised901Black">"Oops!"</div>
     </div>
     <div class="row">
-        <div class="col-sm-1">&nbsp;</div>
-        <div class="col-sm-11 Incised901Light">Commander Shannon Foraker, <em>Ashes of Victory</em><br /><br /></div>
+        <div class="col-sm-2">&nbsp&nbsp;</div>
+        <div class="col-sm-8 Incised901Light">Commander Shannon Foraker, <em>Ashes of Victory</em><br/><br/></div>
     </div>
     <div class="row">
-        <div class="col-sm-12 text-center"><img src="{!!asset('seals/RMN.svg')!!}" width="50%" height="50%"/></div>
+        <div class="col-sm-10 text-center"><img src="{!!asset('seals/RMN.svg')!!}" width="30%" height="30%"/></div>
     </div>
     <div class="row">
-        <div class="col-sm-12 Incised901Bold"><br /><em>Actually, Sir Horace and his team of crackerjack cyberneticists are busy installing new molycircs.  Please be patient while they finish and return MEDUSA to full operation.</em></div>
+        <div class="col-sm-10 Incised901Bold"><br/><em>Actually, Sir Horace and his team of crackerjack cyberneticists
+                are busy installing new molycircs. Please be patient while they finish and return MEDUSA to full
+                operation.</em>
+        </div>
     </div>
 </div>
 </body>
