1a018687e825d442ce1153aa0546e7b30bf2de86
Timberwoof Lupindo <opencollar@timberwoof.com>
2018-11-03 20:47:43 -0700

    Update AwardQualification.php
    Automated code check hated space between close-parens.

--- app/Awards/AwardQualification.php
+++ app/Awards/AwardQualification.php
@@ -233,7 +233,7 @@ trait AwardQualification
         if (is_null($swpQual) === false &&
             in_array($this->branch, $swpBranches) === true &&
             ($swpType === 'Enlisted' && $this->hasAward('ESWP') === false ||
-            $swpType === 'Officer' && $this->hasAward('OSWP') === false) ) {
+            $swpType === 'Officer' && $this->hasAward('OSWP') === false)) {
             // Only process if the qualifications are defined,  it's a branch we check,
             // and they don't have their specific E|O SWP
 
