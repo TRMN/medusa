de66e4b089942257d22d29fb38a7699131e77229
Dave <david.l.weiner30030@gmail.com>
2019-03-07 20:56:59 -0500

    Non-automated updates needed

--- config/logging.php
+++ config/logging.php
@@ -40,7 +40,7 @@ return [
 
         'single' => [
             'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
+            'path' => storage_path('logs/medusa.log'),
             'level' => 'debug',
         ],
 
