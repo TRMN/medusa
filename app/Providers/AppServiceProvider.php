37513ea1985e887dd10dac4cb9c42cab3c425721
Dave <david.l.weiner30030@gmail.com>
2019-03-10 20:06:29 -0400

    Hand updates from shift

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Blade::withDoubleEncoding();
+        Blade::withoutDoubleEncoding();
         Paginator::useBootstrapThree();
     }
 
