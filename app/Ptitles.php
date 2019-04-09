8a5c68548db15c3094c1ad0ef1c5c4b3ac087fe4
Laravel Shift <shift@laravelshift.com>
2019-04-09 19:36:33 +0000

    Remove unnecessary table name property
    By convention, Laravel uses the "snake case", plural name of the class as the table name.

--- app/Ptitles.php
+++ app/Ptitles.php
@@ -7,6 +7,4 @@ use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
 class Ptitles extends Eloquent
 {
     protected $fillable = ['title', 'code', 'precedence'];
-
-    protected $table = 'ptitles';
 }
