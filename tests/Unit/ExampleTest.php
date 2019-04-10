a1f6d7a0576cc3ccaca976ef6da04abc17561291
Timberwoof <gitwoof@timberwoof.com>
2019-04-09 19:42:13 -0700

    some very basic tests
    demonstrates access into some basic functions
    calls functions in app/Billet and app/Branches

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -21,6 +21,19 @@
         {
             Log::debug('Unit/ExampleTest testBasicTest');
             $this->assertTrue(true);
+            
+            $billets = \App\Billet::getBillets();
+            Log::debug('Unit/ExampleTest count($billets):' . count($billets));
+            $this->assertTrue(count($billets) > 500);
+            
+            $branches = \App\Branch::getBranchList();
+            Log::debug('Unit/ExampleTest count($branches):' . count($branches));
+            $this->assertTrue(count($branches) > 10);
+
+            $navalBranches = \App\Branch::getNavalBranchList();
+            Log::debug('Unit/ExampleTest count($navalBranches):' . count($navalBranches));
+            $this->assertTrue(count($branches) > 5);
+            
             Log::debug('Unit/ExampleTest testBasicTest done');
         }
     }
