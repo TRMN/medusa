03bfdb886fe8ce325bf9476ac2ed4c8ea6a3f2eb
Timberwoof <gitwoof@timberwoof.com>
2019-04-25 20:17:19 -0700

    Added first pass at test of getWelcomeLetter.

--- tests/Unit/MedusaUtilityTest.php
+++ tests/Unit/MedusaUtilityTest.php
@@ -17,10 +17,9 @@
          *
          * @return void
          */
-        public function testBasicTest()
+        public function testOrdinal()
         {
-            Log::debug('Unit/MedusaUtilityTest');
-            $this->assertTrue(true);
+            Log::debug('Unit/MedusaUtilityTest/testOrdinal');
             
             // dead-simple one-value test
             $testInt = 10;
@@ -51,4 +50,37 @@
             
             Log::debug('Unit/MedusaUtilityTest done');
         }
+        
+        /**
+         * Test the getWelcomeLetter funciton.
+         */
+        public function testGetWelcomeLetter()
+        {
+            Log::debug('Unit/MedusaUtilityTest/testGetWelcomeLetter');
+            
+            // Get the test user and validate it's who we want.
+            // This will have to replaced with references to a standard test user once the DB is mocked.
+            $timberId = '5b786115a016bd1d8a34cb93';
+            $testUser = \App\User::find($timberId);
+            Log::debug('Unit/MedusaUtilityTest/testGetWelcomeLetter found ' . $testUser->first_name . ' ' . $testUser->last_name);
+            $this->assertTrue($testUser->first_name == 'Timber');
+            $this->assertTrue($testUser->last_name == 'Lupindo');
+
+            // Create a test template; add it to the config.
+            $testTemplate = 'Hello, there. Welcome the %CHAPTER%. Your CO is %CO%';
+            \App\MedusaConfig::set('bupers.welcome', $testTemplate);
+
+            // Run the function under test and log the results
+            $testLetter = \App\Utility\MedusaUtility::getWelcomeLetter($testUser);
+            Log::debug('Unit/MedusaUtilityTest/testGetWelcomeLetter letter:' . $testLetter);
+            
+            // test for some expected values in the letter. 
+            $this->assertTrue(strpos($testLetter, 'Welcome') > 0);
+            $this->assertTrue(strpos($testLetter, 'HMS Medusa') > 0);
+            $this->assertTrue(strpos($testLetter, 'Your CO') > 0);
+            $this->assertTrue(strpos($testLetter, 'Captain (JG)') > 0);
+            $this->assertTrue(strpos($testLetter, 'Erik Roberts') > 0);
+            
+        }
+
     }
