94f8b57d4c9f3b7c33027e91f31385fc7cee7441
Timberwoof <gitwoof@timberwoof.com>
2019-06-03 18:58:05 -0700

    added tests to UserTest.php

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -54,4 +54,65 @@
             $this->assertTrue(is_numeric(substr($llogin,5,2)));
             $this->assertTrue(is_numeric(substr($llogin,8,2)));
         }
+        
+        public function testgetNumExams()
+        {
+            Log::debug('Unit/UserTest testgetNumExams');
+            $numExams = $this->user->getNumExams();
+            Log::debug($numExams);
+            $this->assertTrue($numExams > 10);
+        }
+
+        public function testrouteNotificationForMail()
+        {
+            Log::debug('Unit/UserTest testrouteNotificationForMail');
+            $emailAddy = $this->user->routeNotificationForMail();
+            Log::debug($emailAddy); //me@timberwoof.com
+            $this->assertTrue(strpos($emailAddy, '@') > 0);
+            $this->assertTrue(strpos($emailAddy, '.') > strpos($emailAddy, '@'));
+        }
+
+        public function testgetEmailForPasswordReset()
+        {
+            Log::debug('Unit/UserTest getEmailForPasswordReset');
+            $emailAddy = $this->user->getEmailForPasswordReset();
+            Log::debug($emailAddy); //me@timberwoof.com
+            $this->assertTrue(strpos($emailAddy, '@') > 0);
+            $this->assertTrue(strpos($emailAddy, '.') > strpos($emailAddy, '@'));
+        }
+
+        public function testgetGreetingAndName()
+        {
+            Log::debug('Unit/UserTest getGreetingAndName');
+            $greeting = $this->user->getGreetingAndName();
+            Log::debug($greeting); //Spacer 2nd Class Timber Lupindo
+            $this->assertTrue(strpos($greeting, 'Spacer') == 0);
+            $this->assertTrue(strpos($greeting, 'Class') > 0);
+            $this->assertTrue(strpos($greeting, 'Timber') > 0);
+            $this->assertTrue(strpos($greeting, 'Lupindo') > 0);
+        }
+        
+        public function testgetFullName()
+        {
+            Log::debug('Unit/UserTest getFullName');
+            $fullName = $this->user->getFullName(true);
+            Log::debug($fullName); // Lupindo, Timber
+            $this->assertTrue(strpos($fullName, 'Timber') > 0);
+            $this->assertTrue(strpos($fullName, 'Lupindo') == 0);
+            $fullName = $this->user->getFullName(false);
+            Log::debug($fullName); // Timber Lupindo
+            $this->assertTrue(strpos($fullName, 'Timber') == 0);
+            $this->assertTrue(strpos($fullName, 'Lupindo') > 0);
+        }
+
+        public function testgetGreeting()
+        {
+            Log::debug('Unit/UserTest getGreeting');
+            $greeting = $this->user->getGreeting();
+            Log::debug($greeting); //Spacer 2nd Class
+            $this->assertTrue(strpos($greeting, 'Spacer') == 0);
+            $this->assertTrue(strpos($greeting, 'Class') > 0);
+        }
+        
+        // 294: getGreetingArray
     }
