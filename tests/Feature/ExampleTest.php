7a7c267050bf9d6a6949adc1a1e2a3695ab52b16
Dave Weiner <david.l.weiner30030@gmail.com>
2019-07-18 14:59:56 -0400

    Can we run a test against a test db on our db server cluster

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -11,10 +11,10 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function testBasicTest()
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
+//    public function testBasicTest()
+//    {
+//        $response = $this->get('/');
+//
+//        $response->assertStatus(200);
+//    }
 }
