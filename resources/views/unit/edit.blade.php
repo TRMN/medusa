b9b994d2861a83eeac141d5d3e20d34cc629cb24
Dave <david.l.weiner30030@gmail.com>
2019-08-03 15:56:04 -0400

    Updating blade plugin package, fixed css on edit form

--- resources/views/unit/edit.blade.php
+++ resources/views/unit/edit.blade.php
@@ -11,52 +11,58 @@
 
     {!! Form::model( $chapter, [ 'route' => [ $route . '.update', $chapter->_id ], 'method' => 'put' ] ) !!}
     <div class="row">
-        <div class="col-sm-6  ninety Incised901Light ">
-            {!! Form::label('chapter_name', 'Command or Unit Name') !!} {!! Form::text('chapter_name') !!}
+        <div class="col-sm-6  ninety Incised901Light form-group">
+            {!! Form::label('chapter_name', 'Command or Unit Name') !!} {!! Form::text('chapter_name', $chapter->chapter_name, ['class' => 'form-control']) !!}
         </div>
     </div>
     {!! $branches !!}
     <div class="row">
-        <div class="col-sm-6  ninety Incised901Light ">
+        <div class="col-sm-6  ninety Incised901Light form-group">
             {!! Form::label('Chapter Type', 'Command/Unit Type') !!} {!! Form::select('chapter_type', $chapterTypes, $chapter->chapter_type, ['class' => 'selectize']) !!}
         </div>
     </div>
     <div class="row">
-        <div class="col-sm-6  ninety Incised901Light ">
-            {!! Form::label('hull_number', 'Command/Unit Designation') !!} {!! Form::text('hull_number') !!}
+        <div class="col-sm-6  ninety Incised901Light form-group">
+            {!! Form::label('hull_number', 'Command/Unit Designation') !!} {!! Form::text('hull_number', $chapter->hull_number, ['class' => 'form-control']) !!}
         </div>
     </div>
     <div class="row">
-        <div class="col-sm-6  ninety Incised901Light ">
+        <div class="col-sm-6  ninety Incised901Light form-group">
             {!! Form::label('Assigned To', 'Assigned To') !!} {!! Form::select('assigned_to', $chapterList, $chapter->assigned_to, ['class' => 'selectize']) !!}
         </div>
     </div>
 
     <div class="row">
-        <div class="col-sm-6  ninety Incised901Light ">
-            {!! Form::label('commission_date', 'Stand-up Date') !!}  {!!Form::date('commission_date')!!}
+        <div class="col-sm-6  ninety Incised901Light form-group">
+            {!! Form::label('commission_date', 'Stand-up Date') !!}  {!!Form::date('commission_date', $chapter->commission_date, ['class' => 'form-control'])!!}
         </div>
     </div>
 
     <div class="row">
-        <div class="col-sm-6  ninety Incised901Light ">
+        <div class="col-sm-6  ninety Incised901Light form-group">
             {!! Form::label('decommission_date', 'Stand-down Date') !!}
             @if($numCrew > 0)
                 <p>Unable to set the stand-down date as there are members, other commands or units still assigned
                     to {!!$chapter->chapter_name!!}</p>
             @else
-                {!!Form::date('decommission_date')!!}
+                {!!Form::date('decommission_date', $chapter->decommission_date, ['class' => 'form-control'])!!}
             @endif
 
         </div>
     </div>
 
     <div class="row">
-        <div class="col-sm-6  ninety Incised901Light ">
-            {!!Form::checkbox('joinable', true) !!} New members and transfers may select this unit
+        <div class="col-sm-6  ninety Incised901Light form-group">
+            {!!Form::checkbox('joinable', true, $chapter->joinable) !!} New members and transfers may select this unit
         </div>
     </div>
 
-    <a class="btn" href="{!! URL::previous() !!}">Cancel</a> {!! Form::submit( 'Save', [ 'class' => 'btn' ] ) !!}
+    <div class="row">
+        <div class="col-sm-6 Incised901Light text-center">
+            <a class="btn btn-warning" href="{!! URL::previous() !!}"><span class="fa fa-times"></span> Cancel </a>
+            <button type="submit" class="btn btn-success"><span class="fa fa-save"><span
+                            class="Incised901Light"> Save </span></span></button>
+        </div>
+    </div>
     {!! Form::close() !!}
 @stop
