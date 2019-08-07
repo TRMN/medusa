9771aad60483d63667e159f16a95d92108209e44
Dave Weiner <david.l.weiner30030@gmail.com>
2019-08-07 10:57:14 -0400

    Fix RMACS pay grades

---
+++ database/migrations/2019_08_07_104308_fix_rmacs_paygrades.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use App\Grade;
+
+class FixRmacsPaygrades extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $paygrades = [
+          'C-1' => 'Trainee',
+          'C-2' => null,
+          'C-3' => null,
+          'C-4' => 'Petty Officer Third Class',
+          'C-5' => 'Petty Officer Second Class',
+          'C-6' => 'Petty Officer First Class',
+          'C-7' => 'Chief Petty Officer',
+          'C-8' => 'Senior Chief Petty Officer',
+          'C-9' => 'Master Chief Petty Officer',
+          'C-10' => null,
+          'C-11' => null,
+          'C-12' => 'Ensign',
+          'C-13' => 'Lieutenant (JG)',
+          'C-14' => 'Lieutenant (SG)',
+          'C-15' => 'Lieutenant Commander',
+          'C-16' => 'Commander',
+          'C-17' => 'Captain',
+          'C-18' => 'Commodore',
+          'C-19' => 'Rear Admiral',
+          'C-20' => 'Vice Admiral',
+          'C-21' => 'Admiral',
+          'C-22' => 'Transport Minister',
+          'C-23' => 'Home Secretary',
+        ];
+
+        foreach ($paygrades as $paygrade => $title) {
+          $rec = Grade::where('grade', $paygrade)->first();
+          $ranks = $rec->rank;
+          if (is_null($title)) {
+            unset($ranks['RMACS']);
+          } else {
+            $ranks['RMACS'] = $title;
+          }
+
+          $rec->rank = $ranks;
+          $rec->save();
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
