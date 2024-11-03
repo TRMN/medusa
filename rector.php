bd9ab5ba1dd67b2657bafff40062d5a1ee4a3238
Douglas Needham <douglas.w.needham@gmail.com>
2024-11-02 21:05:57 -0400

    Migrate from PHP 7.3 to 7.4
    - Update composer.json PHP version.
    - Set migration to 7.4
    - Migrate
    - Add log file with results of migration
    
    This was a noop.

--- rector.php
+++ rector.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\SetList;
 use Rector\TypeDeclaration\Rector\Closure\AddClosureVoidReturnTypeWhereNoReturnRector;
+use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -18,6 +19,9 @@ return RectorConfig::configure()
     ])
     // uncomment to reach your current PHP version
     // ->withPhpSets()
-    ->withSets([SetList::PHP_73])
-    ->withSkip([AddClosureVoidReturnTypeWhereNoReturnRector::class])
+    ->withSets([SetList::PHP_74])
+    ->withSkip([
+	AddClosureVoidReturnTypeWhereNoReturnRector::class,
+	ClosureToArrowFunctionRector::class,
+    ])
     ->withTypeCoverageLevel(0);
