ecfce63bb28a4234d53a40fd2a68f1246fba3cc9
Douglas Needham <cinnion@gmail.com>
2025-02-08 19:53:36 -0500

    Changes to get containers working
    - Add a php.ini
    - Add mongodb user/pass to .env file.
    - Fix path in nginx config file.
    - Install gnupg and mongodb in web container.
    - Only make directories under storage if they don't exist.
    - Touch and chmod the medusa log file while making storage directories.
    - Do composer install with --no-cache
    - Fix volumes in docker compose file.
    
    NOTE: Need to get artisan optimize and artisan cache:clear working in start
    script.

--- Docker/web/start.sh
+++ Docker/web/start.sh
@@ -9,15 +9,20 @@ erubis /var/www/html/Docker/web/env.erb > /var/www/html/.env
 
 cd /var/www/html
 
-mkdir storage/framework
-mkdir storage/framework/cache
-mkdir storage/framework/sessions
-mkdir storage/framework/views
-chmod 777 storage/framework/{cache,sessions,views}
+if [ ! -d storage/framework ]
+then
+    mkdir -p storage/framework
+    mkdir -p storage/framework/cache
+    mkdir -p storage/framework/sessions
+    mkdir -p storage/framework/views
+    chmod 777 storage/framework/{cache,sessions,views}
+    touch storage/logs/medusa.log
+    chmod 666 storage/logs/medusa.log
+fi
 
 
 ## Make sure we have the latest packages installed
-composer install
+composer --no-cache install
 
 ## Make sure we have the latest JS packages installed
 npm install
@@ -25,7 +30,12 @@ npm install
 ## Generate the App key
 php artisan key:generate
 
+## Optimize the packages
+#php artisan optimize
+
+## Clear the cache
+#php artisan cache:clear
+
 ## Run the database migrations
-php artisan migrate
 
 php-fpm
