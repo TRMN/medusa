33407ddde24141a6c3ff7195f15b4522acdc7738
Douglas Needham <douglas.w.needham@gmail.com>
2025-09-07 17:24:37 -0400

    Fix table formatting in the emails
    - Remove the indention, which is screwing up the markdown table rendering
      and resulting in a <pre> block with part of the table in it.

--- resources/views/emails/mcamReport.blade.php
+++ resources/views/emails/mcamReport.blade.php
@@ -1,16 +1,16 @@
 @component('vendor.mail.html.message')
     The following individuals have qualified for the indicated MCAM for {{$mcamReport['report_date']}}
 
-    @component('mail::table')
-        | Name | Member ID | Award Number |
-        |------|:-----------:|:----------:|
-        @if (!empty($mcamReport['MCAM']) || !empty($mcamReport['MCAM']))
-            @foreach($mcamReport['MCAM'] as $line)
-                | {{App\User::getUserByMemberId($line->member_id)->getFullName()}} | {{$line->member_id}} | {{App\Utility\MedusaUtility::ordinal($line->qty)}}|
-            @endforeach
-        @else
-            No MCAM qualifications for {{$mcamReport['report_date']}}
-        @endif
-    @endcomponent
+@component('mail::table')
+    | Name | Member ID | Award Number |
+    |------|:-----------|:----------|
+    @if (!empty($mcamReport['MCAM']) || !empty($mcamReport['MCAM']))
+        @foreach($mcamReport['MCAM'] as $line)
+            | {{App\User::getUserByMemberId($line->member_id)->getFullName()}} | {{$line->member_id}} | {{App\Utility\MedusaUtility::ordinal($line->qty)}}|
+        @endforeach
+    @else
+        No MCAM qualifications for {{$mcamReport['report_date']}}
+    @endif
+@endcomponent
 
 @endcomponent
